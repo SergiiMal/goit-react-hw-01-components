2e46f34bb749dcce0a92fb4cd65f4e04df7822f0
SergiiMal <ser4ester@gmail.com>
2023-03-17 22:12:53 +0200

    add /4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
+import transactions from '../data/transactions.json';
 import friends from '../data/friends.json';
 import data from '../data/data.json';
 import user from '../data/user.json';
+import { TransactionHistory } from './transactionHistory/TransactionHistory';
 import { FriendsList } from './friends/FriendsList';
 import { Statistics } from './statistics/Statictics';
 import { Profile } from './profile/Profile';
@@ -15,7 +17,8 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={data} />
-      <FriendsList friends={friends} />;
+      <FriendsList friends={friends} />
+      <TransactionHistory items={transactions} />;
     </>
   );
 };
