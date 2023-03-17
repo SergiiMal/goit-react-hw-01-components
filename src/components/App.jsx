b9f7b6a8108a6a01df71f89a625d209be91626b8
SergiiMal <ser4ester@gmail.com>
2023-03-17 17:01:00 +0200

    add statist and frend

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,21 @@
+import friends from '../data/friends.json';
+import data from '../data/data.json';
 import user from '../data/user.json';
+import { FriendsList } from './friends/FriendsList';
+import { Statistics } from './statistics/Statictics';
 import { Profile } from './profile/Profile';
 export const App = () => {
   return (
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendsList friends={friends} />;
+    </>
   );
 };
